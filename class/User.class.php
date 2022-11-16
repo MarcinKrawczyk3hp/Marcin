86293010a66992ab3c0c5601696e700c7006aa28
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-16 14:07:06 +0100

    :3

--- class/User.class.php
+++ class/User.class.php
@@ -10,7 +10,18 @@
         public function __construct(string $login, string $password)
         {
            $this->login = $login;
-           $this->password = $password;     
+           $this->password = $password;
+           $this->firstName = $firstName ="";
+           $this->lastName = $lastName = "";
+
+        }
+        public function register() {
+            $password = password_hash($this->password, PASSWORD_ARGON2I);
+            $query = "INSERT INTO user VALUES (NULL,?,?,?,?)";
+            $db = new mysqli('localhost', 'root', '', 'loginForm');
+            $preparedQuery = $db->prepare($query);
+            $preparedQuery->bind_param('ssss',$this->login, $passwordHash, $this->firstName, $this->lastName);
+            $preparedQuery->execute();
         }
         }
 ?>
