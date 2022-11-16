a88f532512b00cbe6bab163aec6a8f273ca7fd92
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-16 14:11:55 +0100

    :3

--- class/User.class.php
+++ class/User.class.php
@@ -17,10 +17,10 @@
         }
         public function register() {
             $password = password_hash($this->password, PASSWORD_ARGON2I);
-            $query = "INSERT INTO user VALUES (NULL,?,?,?,?)";
+            $query = "INSERT INTO user1 VALUES (NULL,?,?,?,?)";
             $db = new mysqli('localhost', 'root', '', 'loginForm');
             $preparedQuery = $db->prepare($query);
-            $preparedQuery->bind_param('ssss',$this->login, $passwordHash, $this->firstName, $this->lastName);
+            $preparedQuery->bind_param('ssss', $this->login, $passwordHash, $this->firstName, $this->lastName);
             $preparedQuery->execute();
         }
         }
