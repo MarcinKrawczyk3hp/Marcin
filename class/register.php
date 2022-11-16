404654b0ff7b25e5896ce629246e79e941ea8c39
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-16 15:17:22 +0100

    :3

--- class/register.php
+++ class/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logowanie</title>
+    <title>Rejestracja</title>
 </head>
 <body>
     <form action="" method="post">
@@ -12,17 +12,22 @@
         <input type="text" name="login" id="loginID"><br>
         <label for="passwordID">Hasło:</label><br>
         <input type="password" name="password" id="passwordID"><br>
-        <input type="submit" value="Zaloguj">
+        <label for="firstNameID">Imię:</label><br>
+        <input type="text" name="firstName" id="firstNameID"><br>
+        <label for="lastNameID">Nazwisko:</label><br>
+        <input type="text" name="lastName" id="lastNameID"><br>
+        <input type="submit" value="Rejestruj">
     </form>
 <?php
 if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
-    require_once('config.php');
     require_once('user/User1.class.php');
     $user = new User($_REQUEST['login'], $_REQUEST['password']);
-    if($user->login()) {
-        echo "Zalogowano poprawnie użytkownika: ".$user->getName();
+    $user->setFirstName($_REQUEST['firstName']);
+    $user->setLastName($_REQUEST['lastName']);
+    if($user->register()) {
+        echo "Zarejestrowano poprawnie";
     } else {
-        echo "Błędny login lub hasło";
+        echo "Błąd rejestracji użytkownika";
     }
 }
 ?>    
