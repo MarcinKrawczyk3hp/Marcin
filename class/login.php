cdea1a51f0150a03e8902a4ba4456c2bd8dc85ee
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-23 12:45:44 +0100

    :3

--- class/login.php
+++ class/login.php
@@ -17,7 +17,7 @@
 <?php
 if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
     require_once('config.php');
-    require_once('user/User1.class.php');
+    require_once('User1.class.php');
     $user = new User($_REQUEST['login'], $_REQUEST['password']);
     if($user->login()) {
         echo "Zalogowano poprawnie użytkownika: ".$user->getName();
