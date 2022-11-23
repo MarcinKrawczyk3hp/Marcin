4d128b4dae3313fe8d04ce03936854c5a9b536e4
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-23 13:27:49 +0100

    Twig Działą ;)

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+require_once('vendor/autoload.php');
+
+//loader to taki pomocnik ładowania szablonów
+$loader = new Twig\Loader\FilesystemLoader('templates');
+//inicjujemy twiga
+$twig = new Twig\Environment($loader);
+
+$db = new mysqli('localhost', 'root', '', 'loginform');
+
+?>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+require_once('config.php');
+
+$twig ->display('test.html.twig')
+?>
