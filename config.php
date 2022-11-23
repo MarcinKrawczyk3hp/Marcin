5d837b70ae98fa64ba380402de66aae38ee604cd
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-23 14:15:11 +0100

    odzdzielenie php od html

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 require_once('vendor/autoload.php');
+require_once('class/User.class.php');
 
 //loader to taki pomocnik ładowania szablonów
 $loader = new Twig\Loader\FilesystemLoader('templates');
--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 require_once('config.php');
 
+
+$v = array('testVariable' => "wartość testowa",);
 $twig ->display('test.html.twig')
 ?>
