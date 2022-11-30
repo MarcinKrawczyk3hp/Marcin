3186b9504b6fd796569b79a133b790905514235e
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-30 14:08:12 +0100

    :3

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
-
 use Steampixel\Route;
 
 require_once('config.php');
 require_once('class/User.class.php');
 
+session_start();
+
 Route::add('/', function() {
-    echo "Strona główna";
+    global $twig;
+    $v = array();
+    if(isset($_SESSION['auth']))
+        if($_SESSION['auth']) {
+            //jesteśmy zalogowani
+            $user = $_SESSION['user'];
+            $v['user'] = $user;
+            
+        }
+    $twig->display('home.html.twig', $v);
+    //echo "<pre>";
+    //var_dump($_SESSION);
 });
 
 Route::add('/login', function() { 
@@ -19,6 +31,9 @@ Route::add('/login', function() {
     if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
         $user = new User($_REQUEST['login'], $_REQUEST['password']);
         if($user->login()) {
+            // tu jest już poprawnie zalogowany użytkownik
+            $_SESSION['auth'] = true;
+            $_SESSION['user'] = $user;
             $v = array(
                 'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
             );
@@ -41,6 +56,7 @@ Route::add('/register', function() {
     if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
         if(empty($_REQUEST['login']) || empty($_REQUEST['password'])
             || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])) {
+                //podano pusty string jako jedną z wymaganych wartości
                 $twig->display('register.html.twig', 
                                 ['message' => "Nie podano wymaganej wartości"]);
                 exit();
@@ -49,9 +65,11 @@ Route::add('/register', function() {
         $user->setFirstName($_REQUEST['firstName']);
         $user->setLastName($_REQUEST['lastName']);
         if($user->register()) {
+            //echo "Zarejestrowano poprawnie";
             $twig->display('message.html.twig', 
                                 ['message' => "Zarejestrowano poprawnie"]);
         } else {
+            //echo "Błąd rejestracji użytkownika";
             $twig->display('register.html.twig', 
                                 ['message' => "Błąd rejestracji użytkownika"]);
         }
@@ -59,6 +77,13 @@ Route::add('/register', function() {
         die("Nie otrzymano danych");
     }
 }, 'post');
+
+Route::add('/logout', function() {
+    global $twig;
+    session_destroy();
+    $twig->display('message.html.twig', 
+                                ['message' => "Wylogowano poprawnie"]);
+});
 
 Route::run('/Marcin');
 ?>
