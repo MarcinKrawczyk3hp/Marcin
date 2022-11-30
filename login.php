ff3a945b5a3fd2f0279871d6b0366f683905a45c
MarcinKrawczyk3hp <pinfix01orginal@gmail.com>
2022-11-30 13:23:43 +0100

    :3

--- login.php
+++ login.php
@@ -6,12 +6,10 @@ if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
     require_once('User.class.php');
     $user = new User($_REQUEST['login'], $_REQUEST['password']);
     if($user->login()) {
-        //echo "Zalogowano poprawnie użytkownika: " .$user->getName();
         $v = array(
             'message' => "zalogowano poprawnie użytkownika: ".$user->getName(),
         );
     } else {
-        //echo "Błędny login lub hasło";
         $twig->display('message.html.twig', ['message => "Błędny login Lub Hasło']);
     }
 }else {
